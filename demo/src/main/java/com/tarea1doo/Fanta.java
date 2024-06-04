f5573b673407a8d7c54aac33be7e7bce9a077fda
CondorPepinillo <marttapia2023@udec.cl>
2024-06-04 12:27:21 -0400

    Se borro elementos no usados

--- demo/src/main/java/com/tarea1doo/Fanta.java
+++ demo/src/main/java/com/tarea1doo/Fanta.java
@@ -1,19 +1,7 @@
 package com.tarea1doo;
 
 public class Fanta extends Producto{
-    /**
-     * Constructor de la clase Fanta.
-     * @param serie Serie del producto.
-     */
     public Fanta(){
         super(200);
-    }
-    /**
-     * Método que simula el acto de beber el producto.
-     *
-     * @return Nombre del producto.
-     */
-    public String beber(){
-        return new String("fanta");
     }
 }
