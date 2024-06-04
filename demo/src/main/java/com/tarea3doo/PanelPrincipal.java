3047dd575d1dcd07e4551d99b7d932957f9517da
martinezudec <benjamartinez2023@udec.cl>
2024-06-04 06:36:47 -0400

    feat: se avanza en PanelPrincipal.java

--- demo/src/main/java/com/tarea3doo/PanelPrincipal.java
+++ demo/src/main/java/com/tarea3doo/PanelPrincipal.java
@@ -10,39 +10,11 @@ public class PanelPrincipal extends JPanel {
     private PanelExpendedor exp;
 
     public PanelPrincipal () {
-        exp = new PanelExpendedor ();
+        exp = new PanelExpendedor();
         com = new PanelComprador();
-        this.setBackground(Color.white);
-
-        this.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
-
-                // Obtén las coordenadas del clic
-                int x = e.getX();
-                int y = e.getY();
-
-                // Verifica si el clic ocurrió dentro de PanelComprador
-                if (com.getBounds().contains(x, y)) {
-                    com.handleClick(e);
-                }
-
-                // Verifica si el clic ocurrió dentro de PanelExpendedor
-                if (exp.getBounds().contains(x, y)) {
-                    exp.handleClick(e);
-                }
-
-                // Llama a repaint para actualizar la interfaz gráfica
-                repaint();
-            }
-        });
-    }
-
-    @Override
-    public void paintComponent (Graphics g) {
-        super.paintComponent(g);
-        com.paintComponent(g);
-        exp.paintComponent(g);
+        this.setBackground(Color.WHITE);
+        this.setLayout(new GridLayout());
+        this.add(exp, BorderLayout.NORTH);
+        this.add(com, BorderLayout.SOUTH);
     }
 }
